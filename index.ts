07397b6aff4ba142b2e1db825b9851f780e7f4bc
javad roustaei <javad_r_85@yahoo.com>
2018-02-06 14:56:31 +0330

    1.0.2

---
+++ index.ts
@@ -0,0 +1,27 @@
+// export * from './src/index';
+
+
+
+export * from "./src/core.module";
+export * from "./src/components/base.component";
+export * from "./src/components/base-kendo-grid.component";
+export * from "./src/components/breadcrumb.component";
+export * from "./src/components/delete-confirm/delete-confirm.component";
+export * from "./src/components/toolbar-button/toolbar-button.component";
+export * from "./src/interceptors/http-interceptor.service";
+export * from "./src/interceptors/loader-interceptor.service";
+export * from "./src/interceptors/upload-interceptor.service";
+export * from "./src/model/app-config.model";
+export * from "./src/model/toolbar-button";
+export * from "./src/model/breadcrumb.model";
+export * from "./src/model/operation-result.model";
+export * from "./src/model/select-list.model";
+export * from "./src/model/tree.model";
+export * from "./src/pipes/jalali.pipe";
+export * from "./src/pipes/persian-word.pipe";
+export * from "./src/services/app-config.service";
+export * from "./src/services/base.service";
+export * from "./src/services/base-kendo-grid.service";
+export * from "./src/utils/error-handler";
+export * from "./src/utils/loading-manager";
+export * from "./src/utils/notify-manager";
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+// export * from "./core.module";
+// export * from "./components/base.component";
+// export * from "./components/base-kendo-grid.component";
+// export * from "./components/breadcrumb.component";
+// export * from "./components/delete-confirm/delete-confirm.component";
+// export * from "./components/toolbar-button/toolbar-button.component";
+// export * from "./interceptors/http-interceptor.service";
+// export * from "./interceptors/loader-interceptor.service";
+// export * from "./interceptors/upload-interceptor.service";
+
